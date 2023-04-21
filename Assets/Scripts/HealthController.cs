5e9c6efcdd8a43a30156dd840aace99a3fa68072
adrix-10 <adrix1410@gmail.com>
2023-04-21 15:08:23 -0600

    Funcionalidad de ataque en player
    El player ataca al presionar el boton del "fire1", falta agregar el helath controller a los enemigos y hacer que se muevan al aparecer en el scene con el spawn controller

---
+++ Assets/Scripts/HealthController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class HealthController : MonoBehaviour
+{
+    [SerializeField]
+    float maximumHealth = 100.0F;
+    float currentHealth = 0.0F;
+    void Start()
+    {
+        currentHealth = maximumHealth;
+    }
+    public void TakeDamage(float value)
+    {
+        currentHealth -= Mathf.Abs(value);
+        if (currentHealth <= 0.0F)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/MeleeController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeController : MonoBehaviour
+{
+    [SerializeField]
+    Transform attackPoint;
+
+    [SerializeField]
+    float attackRadius;
+
+    [SerializeField]
+    float damage;
+
+    Animator animator;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1")) 
+        {
+            animator.SetTrigger("melee");
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPoint.position, attackRadius);
+    }
+
+    public void OnAttack() 
+    {
+        Collider2D[] colliders = 
+            Physics2D.OverlapCircleAll(attackPoint.position, attackRadius);
+
+        foreach (Collider2D collider in colliders) 
+        {
+            HealthController healthController = collider.GetComponent<HealthController>();
+            if (healthController != null)
+            {
+                healthController.TakeDamage(damage);
+            }
+        }
+    }
+
+}
